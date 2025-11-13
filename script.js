6e186e4e3082bced37c30061d7d96c4c33014687
brunowigers <bruno.wigers@escola.pr.gov.br>
2025-11-13 20:47:02 +0000

    tá indo

---
+++ script.js
@@ -0,0 +1,15 @@
+const lampada = document.getElementById("lampada");
+
+lampada.addEventListener("mouseover",acender);
+lampada.addEventListener("mouseout",apagar);
+
+function acender(){
+    lampada.src = "on.png";
+    document.body.style.background = "#ffeb3b22"
+}
+
+
+function apagar(){
+    lampada.src = "off.png";
+    document.body.style.background = "#222"
+}
